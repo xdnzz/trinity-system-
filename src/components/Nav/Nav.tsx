7dd50db1bd058835c922b851ca7d48cc52977a57
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-13 12:25:39 -0300

    style: edit code

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,7 +4,7 @@ export function Nav() {
   return (
     <nav className="w-full h-12 bg-slate-600">
       <div className="w-full h-full max-w-7xl m-auto flex items-center justify-between text-white">
-        <div ><FaBeer /></div>
+        <div><FaBeer /></div>
 
         <input className="w-96 h-9 rounded-md placeholder:px-4"
           placeholder="Pesquisar no sistema" />
