6fcb21273f8c6db3a82d48818e3714ede452f825
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-13 12:14:20 -0300

    feat: change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Routes } from "./routes/route";
 import "./global.css";
+import { Nav } from "./components/Nav/Nav";
 
 
 
 function App() {
 
   return (
-    <div className="App w-screen h-screen bg-slate-600  flex flex-row justify-center">
+    <div className="App w-screen h-screen   flex flex-row justify-center">
       <main className="w-[85.375rem] justify-center items-center">
+        <Nav />
         <Routes />
       </main>
     </div>
