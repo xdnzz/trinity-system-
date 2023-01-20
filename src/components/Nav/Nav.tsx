0da90d8ecebe2641e1c050f443eb24e68e978035
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-20 14:30:28 -0300

    style: change navbar height

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,8 +2,8 @@ import { FaBeer, FaBell, FaUserAlt } from "react-icons/fa"
 
 export function Nav() {
   return (
-    <nav className="w-full h-12 bg-slate-600">
-      <div className="w-full h-full max-w-7xl m-auto flex items-center justify-between text-white">
+    <nav className="w-full h-20 bg-slate-600">
+      <div className="w-full h-12 max-w-7xl m-auto flex items-center justify-between text-white">
         <div><FaBeer/></div>
         <input
           className="w-96 h-9 rounded-md placeholder:px-4  text-zinc-800 p-2"
