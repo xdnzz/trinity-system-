3ec7189708c0f81b777c695a4b6407fc4bb7463d
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-20 11:59:01 -0300

    style: change code indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes } from "./routes/route";
 import "./global.css";
 import { Nav } from "./components/Nav/Nav";
-import { Header } from "./components/Header/Header";
+
 
 
 
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App w-screen h-screen flex flex-col items-center">
-            <Nav />
+      <Nav />
       <main className="w-[85.375rem] justify-center items-center">
         <Routes />
       </main>
